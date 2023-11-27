3068e2bdc3d4d298c30a857c71be485719737728
ProjectFrost <113421256+ProjectFrost@users.noreply.github.com>
2023-11-27 22:10:05 +0100

    ez

---
+++ Numeros_primos.py
@@ -0,0 +1,15 @@
+import math
+import sys
+num=int(input("Introduce el numero: "))
+esPrimo=True;
+
+if num==1:
+    esPrimo=False
+    print("Es el numero primo: "+ str(esPrimo))
+    sys.exit(0)
+
+for divisor in range(2,int(math.sqrt(num)+1)):
+   if (num%divisor==0):
+        esPrimo=False
+        break
+print("Es el numero primo: "+ str(esPrimo))
