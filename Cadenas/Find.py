3c6d7d75ec91023df953ac4b0ec4f593ee07086b
ProjectFrost <113421256+ProjectFrost@users.noreply.github.com>
2023-12-04 13:06:48 +0100

    Update Find.py

--- Cadenas/Find.py
+++ Cadenas/Find.py
@@ -2,7 +2,7 @@ cadena=input("Introduce la cadena: ")
 subcadena=(input("Introduce el caracter a encontrar: "))[:1] #Coje el primer caracter
 contador=0
 
-for i in range(0,len(cadena)):
+for i in cadena:
     if subcadena == cadena[i]:
         contador+=1
         print(f"Posicion en la cadena --> {i}")
