37bc776fc5edfb9f459500d1f9a4ff5093023c2e
ProjectFrost <113421256+ProjectFrost@users.noreply.github.com>
2023-12-04 13:05:38 +0100

    ye

--- Cadenas/Find.py
+++ Cadenas/Find.py
@@ -1,10 +1,11 @@
 cadena=input("Introduce la cadena: ")
-subcadena=(input("Introduce el caracter a encontrar: "))[:1]
+subcadena=(input("Introduce el caracter a encontrar: "))[:1] #Coje el primer caracter
 contador=0
 
 for i in range(0,len(cadena)):
     if subcadena == cadena[i]:
         contador+=1
+        print(f"Posicion en la cadena --> {i}")
             
 
 print(f"El caracter {subcadena} aparece {contador} veces")
